c51d381969915a5f0762893e2550f079e6225ef4
Martin Achtermann <mdacht777@gmail.com>
2020-06-06 22:16:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,9 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(str)
+  if str=="I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!

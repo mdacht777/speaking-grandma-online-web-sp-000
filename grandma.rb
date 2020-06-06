2ee9d3b42a8ac0e12db0387f9e31cd0671f86b8b
Martin Achtermann <mdacht777@gmail.com>
2020-06-06 22:18:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
+  if str==upcase(str)
+    return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP SONNY!"
+  end
   if str=="I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
